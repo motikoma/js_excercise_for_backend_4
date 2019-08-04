d34bb2faa2bca98e23799149d25718f0e046691f
motikoma <motikoma5125@gmail.com>
2019-08-04 20:24:33 +0900

    環境構築

---
+++ src/QuizFetcher.js
@@ -0,0 +1,5 @@
+// src/QuizFetcher.js」は以下の機能を持つ
+//  fetchという名前の クラスメソッド を持つ
+//  fetch メソッド( クラスメソッド )は、axiosで、以下のURLを使ってデータを取得する
+// https://opentdb.com/api.php?amount=10&type=multiple
+//  fetchメソッドの戻り値は、axios.getメソッドの実行結果の中に含まれるdataプロパティを返す
---
+++ test/src/QuizFetcher.spec.js
@@ -0,0 +1,13 @@
+// 「test/src/QuizFetcher.spec.js」内で、QuizFetcher.fetch メソッドで取得するデータとデータ形式をテストする
+//  results プロパティを持つ
+//  results プロパティはは10件データをもつ配列である
+//  results プロパティの配列の中身は全てオブジェクトで、次のプロパティを持つ
+//  category プロパティ : 文字列型
+//  type プロパティ : 文字列型
+//  difficulty プロパティ : 文字列型
+//  question プロパティ : 文字列型
+//  correct_answer プロパティ : 文字列型
+//  incorrect_answers プロパティ : 3件の文字列を含む配列
+//  typeof を使って、書くプロパティ値の型をテストする
+
+// const assert = require("power_assert");
